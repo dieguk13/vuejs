0354854ddbec590d37bb934837b04d9954ad357b
diego londoño <diegoferlondo13@gmail.com>
2022-11-14 18:19:31 -0500

    cambio definido

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
+
+<script setup>
+const name = "vue dinamico"
+
+</script>
 <template>
-  <h1>esto es un merge</h1>
+  <h1>esto es un merge con {{ name}}</h1>
 </template>
+
+<style>
+h1 {
+color: red;
+} 
+</style>
